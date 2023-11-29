cb0b4738cf4f3f9b7c9822810b643693a434011e
pooulad <poooooladi@gmail.com>
2023-11-29 15:36:35 +0330

    implement game logic

--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+)
 
-func main(){
-	fmt.Println("ready to go now :)")
+func main() {
+	gameWords := []string{"golang", "javascript", "php", "rust", "python"}
+	randomWord := gameWords[rand.Intn(len(gameWords))]
+	lives := 5
+
+	blanks := []string{}
+	for range randomWord {
+		blanks = append(blanks, "_")
+	}
+
+	for {
+		fmt.Printf("❤️ %d, Word letters : %s\n", lives, strings.Join(blanks, " "))
+
+		var input string
+		fmt.Scanln(&input)
+
+		if len(input) > len(randomWord) {
+			fmt.Println("your input length is bigger than word length")
+			lives--
+		} else {
+			for _, inputLetter := range input {
+				correctAnswer := false
+				for i, wordLetter := range randomWord {
+					if inputLetter == wordLetter {
+						blanks[i] = string(inputLetter)
+						correctAnswer = true
+					}
+				}
+				if !correctAnswer {
+					lives--
+				}
+			}
+
+			if lives <= 0 {
+				fmt.Printf("💔 0, Word: %s - sorry you lost.\n", randomWord)
+			}
+
+			if randomWord == strings.Join(blanks, "") {
+				fmt.Printf("❤️ %d, Word: %s - you won.\n", lives, randomWord)
+			}
+		}
+	}
 }
